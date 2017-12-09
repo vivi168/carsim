32310b0480fb311510082ffa4df153e9292b9ac1
Vivien B <vivienbihl@gmail.com>
2017-12-09 20:34:57 +0100

    yapf pass

--- simulator.py
+++ simulator.py
@@ -6,6 +6,7 @@ import inspect
 import fractions
 import argparse
 
+
 class Constant:
   RHO = 1.205
   PI = math.pi
@@ -14,27 +15,34 @@ class Constant:
   MI_PER_M = 0.000621371
   MPH_PER_MS = 2.23694
 
+
 class ActsAsDict:
-    def __init__(self):
-        pass
-    def init_args(self):
-        return inspect.getargspec(self.__init__).args[1:]
-    def keys(self):
-        return self.init_args()
-    def __getitem__(self, key):
-        return getattr(self, key)
+  def __init__(self):
+    pass
+
+  def init_args(self):
+    return inspect.getargspec(self.__init__).args[1:]
+
+  def keys(self):
+    return self.init_args()
+
+  def __getitem__(self, key):
+    return getattr(self, key)
+
 
 class Wheel(ActsAsDict):
   def __init__(self, section_width, aspect_ratio, rim_diameter, mu, pressure):
-    self.radius = ((rim_diameter * Constant.CM_PER_INCH / 2) + (section_width/10 * aspect_ratio/100)) / 100 # m
-    self.tire_mu = mu # tire friction coefficient
-    self.tire_pressure = pressure # tire pressure, bar
+    self.radius = ((rim_diameter * Constant.CM_PER_INCH / 2) +
+                   (section_width / 10 * aspect_ratio / 100)) / 100  # m
+    self.tire_mu = mu  # tire friction coefficient
+    self.tire_pressure = pressure  # tire pressure, bar
+
 
 class Transmission(ActsAsDict):
   def __init__(self, ratios, diff_ratios, friction_loss):
-    self.ratios = ratios # gears ratios
-    self.diff_ratios = diff_ratios # differential ratio
-    self.friction_loss = friction_loss # gearbox friction loss
+    self.ratios = ratios  # gears ratios
+    self.diff_ratios = diff_ratios  # differential ratio
+    self.friction_loss = friction_loss  # gearbox friction loss
     self.current_gear = 1
 
   def get_ratio(self, for_gear=None):
@@ -54,11 +62,12 @@ class Transmission(ActsAsDict):
     if self.current_gear < len(self.ratios):
       self.current_gear += 1
 
+
 class Engine(ActsAsDict):
   def __init__(self, rpm_p, torque_p):
 
-    self.rpm_p = rpm_p # list of rpm values, x axis
-    self.torque_p = torque_p # list of torque values, y axis
+    self.rpm_p = rpm_p  # list of rpm values, x axis
+    self.torque_p = torque_p  # list of torque values, y axis
     self.current_rpm = 0
 
   def get_torque(self, for_rpm=None):
@@ -68,12 +77,14 @@ class Engine(ActsAsDict):
     # linear interpolation to get torque for a given rpm
     return np.interp(for_rpm, self.rpm_p, self.torque_p)
 
-class Car:
-  def __init__(self, mass, C_d, A, launch_rpm, redline_rpm, engine, transmission, wheel):
 
-    self.mass = mass # vehicle mass, kg
-    self.C_d = C_d # drag coefficient
-    self.A = A # car frontal area
+class Car:
+  def __init__(self, mass, C_d, A, launch_rpm, redline_rpm, engine,
+               transmission, wheel):
+
+    self.mass = mass  # vehicle mass, kg
+    self.C_d = C_d  # drag coefficient
+    self.A = A  # car frontal area
 
     self.launch_rpm = launch_rpm
     self.redline_rpm = redline_rpm
@@ -82,15 +93,16 @@ class Car:
     self.transmission = Transmission(**transmission)
     self.engine = Engine(**engine)
 
-    self.a = 0 # acceleration, m.s^-2
-    self.v = 0 # velocity, m.s^-1
-    self.s = 0 # position, m
+    self.a = 0  # acceleration, m.s^-2
+    self.v = 0  # velocity, m.s^-1
+    self.s = 0  # position, m
 
   def F_drag(self):
     return 0.5 * self.C_d * self.A * Constant.RHO * self.v**2
 
   def C_rr(self):
-    return 0.005 + (1 / self.wheel.tire_pressure) * (0.01 + 0.0095 * (self.v*3.6 / 100)**2)
+    return 0.005 + (1 / self.wheel.tire_pressure) * (0.01 + 0.0095 *
+                                                     (self.v * 3.6 / 100)**2)
 
   def F_rr(self):
     return self.C_rr() * self.mass * Constant.G
@@ -105,24 +117,31 @@ class Car:
       data = json.load(car_data)
       return Car(**data)
 
+
 class Logger:
-  def __init__(self, speed_points=[60, 100, 120, 200], position_points=[0.25, 0.5, 1]):
+  def __init__(self,
+               speed_points=[60, 100, 120, 200],
+               position_points=[0.25, 0.5, 1]):
     self.speed_points = speed_points
     self.position_points = position_points
-    self.speed_summary = {p:None for p in speed_points}
-    self.position_summary = {p:None for p in position_points}
+    self.speed_summary = {p: None for p in speed_points}
+    self.position_summary = {p: None for p in position_points}
 
   def __str__(self):
     summary = []
     for p in self.speed_points:
       if self.speed_summary[p]:
-        summary.append('{:<12} {:<6.2f} seconds'.format("0-%s mph" % p, self.speed_summary[p]))
+        summary.append('{:<12} {:<6.2f} seconds'.format(
+            "0-%s mph" % p, self.speed_summary[p]))
 
     for p in self.position_points:
       if self.position_summary[p]:
         summary.append(len(summary[0]) * '-')
-        summary.append('{:<12} {:<6.2f} seconds'.format(str(fractions.Fraction(p)) + " mi", self.position_summary[p][0]))
-        summary.append('{:<12} {:<6.2f} mph'.format(str(fractions.Fraction(p)) + " mi spd", self.position_summary[p][1]))
+        summary.append('{:<12} {:<6.2f} seconds'.format(
+            str(fractions.Fraction(p)) + " mi", self.position_summary[p][0]))
+        summary.append('{:<12} {:<6.2f} mph'.format(
+            str(fractions.Fraction(p)) + " mi spd",
+            self.position_summary[p][1]))
 
     return "\n".join(summary)
 
@@ -137,29 +156,37 @@ class Logger:
 
   def check_position(self, s, v, t):
     for p in self.position_points:
-      if not self.position_summary[p] and (s * Constant.MI_PER_M)  > p:
+      if not self.position_summary[p] and (s * Constant.MI_PER_M) > p:
         self.position_summary[p] = [t, v * Constant.MPH_PER_MS]
+
 
 def main(car_file):
   car = Car.from_json(car_file)
 
-  t_step = 0.01 # time step, s
-  t_max = 360.0 # end time, s
+  t_step = 0.01  # time step, s
+  t_max = 360.0  # end time, s
   t = 0
 
   logger = Logger()
 
-  result = [["time", "RPM", "gear", "T_eng", "T_wheel", "F_wheel", "F_wheel_max", "F_drive", "F_drag", "F_rr", "F_net", "a", "v", "s"]]
+  result = [[
+      "time", "RPM", "gear", "T_eng", "T_wheel", "F_wheel", "F_wheel_max",
+      "F_drive", "F_drag", "F_rr", "F_net", "a", "v", "s"
+  ]]
 
   while t < t_max:
-    car.engine.current_rpm = (car.v / car.wheel.radius) * car.transmission.get_ratio() * 60 / (2*Constant.PI)
+    car.engine.current_rpm = (
+        car.v / car.wheel.radius) * car.transmission.get_ratio() * 60 / (
+            2 * Constant.PI)
 
     if car.transmission.current_gear == 1 and car.engine.current_rpm < car.launch_rpm:
       car.engine.current_rpm = car.launch_rpm
 
     if car.engine.current_rpm > car.redline_rpm:
       car.transmission.shift_up()
-      car.engine.current_rpm = (car.v / car.wheel.radius) * car.transmission.get_ratio() * 60 / (2*Constant.PI)
+      car.engine.current_rpm = (
+          car.v / car.wheel.radius) * car.transmission.get_ratio() * 60 / (
+              2 * Constant.PI)
 
     T_eng = car.engine.get_torque()
     T_wheel = T_eng * car.transmission.get_net_ratio()
@@ -179,7 +206,11 @@ def main(car_file):
     car.v = car.a * t_step + car.v
     car.s = car.v * t_step + car.s
 
-    result.append([t, car.engine.current_rpm, car.transmission.current_gear, T_eng, T_wheel, F_wheel, F_wheel_max, F_drive, F_drag, F_rr, F_net, car.a, car.v, car.s])
+    result.append([
+        t, car.engine.current_rpm, car.transmission.current_gear, T_eng,
+        T_wheel, F_wheel, F_wheel_max, F_drive, F_drag, F_rr, F_net, car.a,
+        car.v, car.s
+    ])
 
     logger.log(car.s, car.v, t)
     t = t + t_step
